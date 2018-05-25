3d53664bc96e212098d4500bc148f1b04c90e06d
Maylen Medina <maylen300@hotmail.com>
2018-05-25 10:10:09 +1000

    building a grid view of images code puzzle

--- iOS try app/ClassicModeViewController.m
+++ iOS try app/ClassicModeViewController.m
@@ -19,35 +19,79 @@
 @implementation ClassicModeViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    /*TESTING PURPOSES
+     NSLog(@"\n\n VIEW DID LOAD : game view width is: %f", gameViewWidth);
+     //this shows in the command line what the width size is. 260 in this
+     case for iphone SE.
+     NSLog(@"\n\n VIEW DID APPEAR : game view width is: %f", gameViewWidth);
+     */
+}
+
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:YES];
     
+    [_gameView layoutIfNeeded];//forces the receiver to layout its subviews inmediately
+                              //if required. it resizes a custom view
+                             //this makes sure of load first the layout then do the calculations
     gameViewWidth = _gameView.frame.size.width;
-    NSLog(@"\n\n game view width is: %f", gameViewWidth);//this shows in the command line what the width size is. 260 in this case.
     
+     [self makeBlocksAction];
     /*
      #proccess involved when calling this method:
      
      //1-It calculates the  float value, a quarter of the width
-        make a new frame for it
-     //2-CGRect blockFrame -> Based on the top left corner based on height and width that we established before
+     make a new frame for it
+     //2-CGRect blockFrame -> Based on the top left corner based on height and
+        width that we established before
      //3-Make a new imageView based on that new frame
      //4-stick the image into the block
      //5-Added to the gameView
      
      */
-    [self makeBlocksAction];
 }
+
 //method that makes the blocks
 -(void)makeBlocksAction
 {
     float blockWidth = gameViewWidth /4;
     
-    //this will generate the frame for us to hold the pieces
-    CGRect blockFrame = CGRectMake(0, 0, blockWidth, blockWidth);
-    UIImageView* block = [[UIImageView alloc] initWithFrame:blockFrame];
-    block.image = [UIImage imageNamed:@"cute_01.jpg"];
-    [_gameView addSubview:block];
+    //this is made because every screen size varies and it needs to
+    //calculate the halfcenter y and x of the UIImageView (block) based on frame
+    float xCen = blockWidth /2;
+    float yCen = blockWidth /2;
+    
+    int imgNum = 1;
+    
+    //horizontla blocks
+    for (int h = 0; h < 4; h++)
+    {
+        
+        //vertical blocks
+        for (int v = 0; v < 4; v++)
+        {
+            CGPoint newCen = CGPointMake(xCen, yCen);
+            
+            //this will generate the frame for us to hold the pieces
+            //-3 because it will generate a bit of margin of height and width between blocks
+            CGRect blockFrame = CGRectMake(0, 0, blockWidth-3, blockWidth-3);
+            UIImageView* block = [[UIImageView alloc] initWithFrame:blockFrame];
+            
+            //whatever integer value its, needs to have 2 digits
+            NSString* imgName = [NSString stringWithFormat:@"cute_%02d.jpg", imgNum];
+            block.image = [UIImage imageNamed: imgName];
+            block.center = newCen;
+            [_gameView addSubview:block];
+            
+            //once you make one block, make the other one and so on
+            xCen += blockWidth;
+            imgNum = imgNum +1;
+        }
+        //once the first round is made do:
+        yCen = yCen + blockWidth; //bring them one step lower
+        xCen = blockWidth /2; //start making the blocks from the left again
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
